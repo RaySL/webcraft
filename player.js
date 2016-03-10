3856322b2e68ee390eae98335aba0a9c4f15f7f0
swax97 <sedna9801@gmail.com>
2016-03-10 14:47:42 +0000

    Proper collisions implemented, invisible/intangible blocks still present

--- player.js
+++ player.js
@@ -10,16 +10,16 @@ function terrain(pos){
     
     
     var rand = vec3.dot(f, new vec3(14.11,86.56,131.43));
-    rand = rand - Math.floor(rand);
+    rand = rand - Math.floor(rand|0);
     
     var x = vec3.dot(f, f)*0.075 - 1.0 - rand * 0.3;
-    return 1.0 - x*x < 0.02;
+    return 1.0 - x*x < 0.0;
 }
 
 function Player(){
     this.direction = new vec3(0.0, 0.0, 1.0);
     
-    this.position = new vec3(0.0, 0.0, 2.0);
+    this.position = new vec3(0.0, 0.0, 3.0);
     this.velocity = new vec3(0.0, 0.0, 0.0);
     
     this.speed = 0.01;
@@ -37,23 +37,55 @@ Player.prototype = {
     
     update: function(){
         this.velocity.scalar_mul(0.9);
-        this.position.add(this.velocity);
+        //this.position.add(this.velocity);
         this.velocity.add(this.gravity);
     },
     
     collide: function(){
+        this.collideX();
+        this.collideY();
+        this.collideZ();
+    },
+    touch: function(){
+        return terrain(new vec3(this.position.x + 0.5,
+                                this.position.y + 0.5,
+                                this.position.z + 0.5)) || 
+               terrain(new vec3(this.position.x - 0.5,
+                                this.position.y - 0.5,
+                                this.position.z - 0.5));
+    },
+    
+    collideX: function(){
+        this.position.x += this.velocity.x;
         
-        if (terrain(new vec3(this.position.x,
-                             this.position.y + 1.5,
-                             this.position.z)) || 
-            terrain(new vec3(this.position.x,
-                             this.position.y - 0.5,
-                             this.position.z))) {
-            this.position.sub(this.velocity);
+        if (this.touch()) {   
+            this.position.x -= this.velocity.x;
+            //this.position.x = Math.floor(this.position.x);
             
-            this.velocity = new vec3(0.0, 0.0, 0.0);
+            this.velocity.x *= -0.1;//new vec3(0.0, 0.0, 0.0);
         }
     },
+    collideY: function(){
+        this.position.y += this.velocity.y;
+        
+        if (this.touch()) {
+            this.position.y -= this.velocity.y;
+            //this.position.x = Math.floor(this.position.x);
+            
+            this.velocity.y *= -0.1;//new vec3(0.0, 0.0, 0.0);
+        }
+    },
+    collideZ: function(){
+        this.position.z += this.velocity.z;
+        
+        if (this.touch()) {
+            this.position.z -= this.velocity.z;
+            //this.position.x = Math.floor(this.position.x);
+            
+            this.velocity.z *= -0.1;//new vec3(0.0, 0.0, 0.0);
+        }
+    },
+    
     move: function(vec){
         this.velocity.add(vec);
     },
