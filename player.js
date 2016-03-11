b30f8cde4b740298e4f0a0ce295e1dad22775661
swax97 <sedna9801@gmail.com>
2016-03-11 04:00:30 +0000

    Added noise, will need to calculate collisions in GLSL, for accuracy

--- player.js
+++ player.js
@@ -1,8 +1,34 @@
 /*global vec3*/
 /*global Input*/
 
+function mix(a, b, v){
+    return a * (1.0 - v) + b * v;
+}
 
-function terrain(pos){
+function hash(n) { n = Math.sin(n)*753.5453123; return n - Math.floor(n); }
+//function hash(n){n -= Math.floor(n); n *= 753.5453123; return n - Math.floor(n);}
+function noise(x, y, z){
+    var px = Math.floor(x);
+    var py = Math.floor(y);
+    var pz = Math.floor(z);
+    
+    var fx = x - px;
+    var fy = y - py;
+    var fz = z - pz;
+    
+    fx = fx*fx*(3.0-2.0*fx);
+    fy = fy*fy*(3.0-2.0*fy);
+    fz = fz*fz*(3.0-2.0*fz);
+	
+    var n = px + py*157.0 + 113.0*pz;
+    return mix(mix(mix( hash(n+  0.0), hash(n+  1.0),fx),
+                   mix( hash(n+157.0), hash(n+158.0),fx),fy),
+               mix(mix( hash(n+113.0), hash(n+114.0),fx),
+                   mix( hash(n+270.0), hash(n+271.0),fx),fy),fz);
+}
+
+
+/*function terrain(pos){
     var fx = Math.floor(pos.x);
     var fy = Math.floor(pos.y);
     var fz = Math.floor(pos.z);
@@ -17,12 +43,19 @@ function terrain(pos){
     
     var x = vec3.dot(f, f)*0.075 * (rand * 0.75 + 1.0) - 1.5;
     return 1.0 - x*x < 0.0;
+}*/
+function terrain(pos){
+    pos.x = Math.floor(pos.x);
+    pos.y = Math.floor(pos.y);
+    pos.z = Math.floor(pos.z);
+    var x = noise(pos.x*0.3, pos.y*0.3, pos.z*0.3) + 0.5;
+    return 1.0 - x*x < 0.0;
 }
 
 function Player(){
     this.direction = new vec3(0.0, 0.0, 1.0);
     
-    this.position = new vec3(0.0, 0.0, 4.0);
+    this.position = new vec3(0.0, 0.0, 0.0);
     this.velocity = new vec3(0.0, 0.0, 0.0);
     
     this.speed = 0.005;
